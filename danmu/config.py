0fdc7d8b7ca1218ac66f4ffeafbb5f5982dcb853
ly645091114 <645091114@qq.com>
2025-12-02 22:22:10 +0800

    feat: 解耦功能文件，区分不同平台弹幕服务逻辑

---
+++ danmu/config.py
@@ -0,0 +1,9 @@
+# config.py
+PLATFORM = "douyu"          # 以后可以支持 "huya", "bilibili" 等
+ROOM_ID = 242737            # 斗鱼房间号
+# ROOM_ID = 88660            # 斗鱼房间号
+OBS_WS_HOST = "127.0.0.1"
+OBS_WS_PORT = 8765
+USE_SOCKS_PROXY = False
+
+TTS_API_URL = "http://127.0.0.1:9522/tts"
